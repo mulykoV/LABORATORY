693117a1467c7f3814c5d108b6ff206b7836a911
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-09-06 20:14:49 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class RNGCryptoServiceProviderDemo
+{
+    static void Main()
+    {
+        Console.WriteLine("Генерація  криптографічно стійкої послідоності випадкових чисел!!!");
+        var rndNumberGenerator = new RNGCryptoServiceProvider();
+        var randomNumber = new byte[64];
+        rndNumberGenerator.GetBytes(randomNumber);
+        Console.WriteLine(Convert.ToBase64String(randomNumber));
+    }
+}
