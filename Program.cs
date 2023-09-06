c3362b86aca6ec15037d8bb7ee27be4e1ecf0a96
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-09-06 19:16:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Генерація псевдовипадкових чисел!");
+        Random random1 = new Random();
+        Console.WriteLine("Будь-ласка, введіть кількість чисел, яку ви хочете отримати!!!");
+        int count1 = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Набір псевдовипадкових чисел");
+        RandomGenerateCrypto(random1, count1);
+       
+    }
+
+    static void RandomGenerateCrypto(Random random, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            int random1 = random.Next(10, 20);
+            
+            Console.WriteLine(random1);
+        }
+    }
+
+    
+    
+
+}
