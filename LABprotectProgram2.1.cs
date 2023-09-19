a170e683dcb5fc9deab359e3cbcb108b94f1911e
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-09-19 17:14:03 +0300

    Add files via upload

---
+++ LABprotectProgram2.1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the text-->");
+        string text = Console.ReadLine();
+        Console.WriteLine("Enter the key-->");
+        string key = Console.ReadLine();
+
+        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+        byte[] encryptedText = new byte[textBytes.Length];
+
+        for (int i = 0; i < textBytes.Length; i++)
+        {
+            encryptedText[i] = (byte)(textBytes[i] ^ keyBytes[i % keyBytes.Length]);
+        }
+
+        using (FileStream stream = new FileStream("data.dat", FileMode.OpenOrCreate))
+        {
+            stream.Write(encryptedText, 0, encryptedText.Length);
+        }
+
+        Console.WriteLine("Text encrypted and written to data.dat.");
+    }
+}
